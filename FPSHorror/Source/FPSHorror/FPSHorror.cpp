b0f924ba40eb839f41246912a0bcc007db02e3e7
Mackenzie Wilson <macky90078@gmail.com>
2016-04-06 11:36:07 -0400

    fixing things. Deleted it all!

--- FPSHorror/Source/FPSHorror/FPSHorror.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#include "FPSHorror.h"
-
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FPSHorror, "FPSHorror" );
- 
--- FPSHorror/Source/FPSHorror/FPSHorror.h
+++
@@ -1,9 +0,0 @@
-// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
-
-#ifndef __FPSHORROR_H__
-#define __FPSHORROR_H__
-
-#include "EngineMinimal.h"
-
-
-#endif
