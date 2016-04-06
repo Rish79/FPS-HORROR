b41248f13745a6264fd33203f5979d73af5ba910
Mackenzie Wilson <macky90078@gmail.com>
2016-04-06 11:43:32 -0400

    Fixing master still. re-adding everything

---
+++ FPSHorror/Source/FPSHorror/FPSHorrorGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2015 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "FPSHorrorGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFPSHorrorGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AFPSHorrorGameMode();
+};
+
+
+
